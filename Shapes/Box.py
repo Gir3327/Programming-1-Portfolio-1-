a26eb8fa0c9b04178e3eaec7051b2f7880b3a6c9
Mikhail Were <mikhailtwere@gmail.com>
2017-05-24 11:21:17 -0600

    Add files via upload

---
+++ Shapes/Box.py
@@ -0,0 +1,9 @@
+#Mikhail Were A3 Box
+w = input("Insert Number")
+h = input("Insert Number")
+d = input("Insert Number")
+print w, h, d
+v = "Volume =", w * h * d
+print v
+sa = "SA = ", 2*(w*d) + 2*(d*h) + 2*(w*h)-450
+print sa
